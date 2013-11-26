def40ab56062dc4fc1043fe1c923b9b77afa0241
Aditya Avinash Atluri <adityaavinash143@gmail.com>
2013-11-26 14:39:25 +0530

    Created sse_store with all store intrinsics for SSE1

---
+++ sse_store.go
@@ -0,0 +1,39 @@
+
+
+package sse_store
+
+/*
+ #include<xmmintrin.h>
+*/
+
+func MM_Store1ps(x *float32, a C.__m128) {
+	C._mm_store1_ps((*C.float)(x), (C.__m128)(a))
+}
+
+func MM_Storeps(x *float32, a C.__m128) {
+	C._mm_store_ps((*C.float)(x), (C.__m128)(a))
+}
+
+func MM_Storeps1(x *float32, a C.__m128) {
+	C._mm_store_ps1((*C.float)(x), (C.__m128)(a))
+}
+
+func MM_Storess(x *float32, a C.__m128) {
+	C._mm_store_ss((*C.float)(x), (C.__m128)(a))
+}
+
+func MM_Storehpi(x *C.__m64, a C.__m128) {
+	C._mm_storeh_pi((*C.__m64)(x), (C.__m128)(a))
+}
+
+func MM_Storelpi(x *C.__m64, a C.__m128) {
+	C._mm_storel_pi((*C.__m64)(x), (C.__m128)(a))
+}
+
+func MM_Storerps(x *float32, a C.__m128) {
+	C._mm_storer_ps((*C.float)(x), (C.__m128)(a))
+}
+
+func MM_Storeups(x *float32, a C.__m128) {
+	C._mm_storeu_ps((*C.float)(x), (C.__m128)(a))
+}
